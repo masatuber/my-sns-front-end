7e1afcd80e2040f723a784645f0f02acaf686322
masattuber <uvervivid1231@icloud.com>
2025-03-13 13:58:32 +0900

    “URL許可する”

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const allowedOrigins = ['http://localhost:5000', 'http://localhost:3000','https://my-sns-backend.onrender.com/'];
+const allowedOrigins = ['http://localhost:5000', 'http://localhost:3000','https://my-sns-backend.onrender.com/', 'https://news-nagara-sns.onrender.com/'];
 
 export default defineConfig({
   plugins: [react()],
